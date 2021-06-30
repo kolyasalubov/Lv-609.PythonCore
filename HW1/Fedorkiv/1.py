bf171a25563a5fde4b7feb9f083468458492401c
Andrii Fedorkiv <andrikk2521@gmail.com>
2021-06-30 22:29:17 +0300

    added HW1&HW3

---
+++ HW1/Fedorkiv/1.py
@@ -0,0 +1,7 @@
+user_name = input('enter your name:')
+print ('hello,', user_name)
+user_age = input('how old are you')
+user_age= int(user_age)
+print ('Your age is', user_age)
+user_live= input('Where do you live?')
+print ('You live in', user_live)
