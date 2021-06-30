ef19ba6a2a66ca9ea0863d85ed4c1e647c4647cc
Yaroslava Yukhyma <brak.fantaziyi@gmail.com>
2021-06-30 14:57:41 +0300

    created HW3, added python_philosophy

--- HW1/Slava_Yukhyma/input_output.py
+++ HW1/Slava_Yukhyma/input_output.py
@@ -1,13 +1,14 @@
-name = input('What is your name?: ')
-age = input('How old are you?: ')
-city = input('Where do you live?: ')
-
-
 def sep(symbol, quantity):
     print(symbol*quantity)
 
+if __name__=='__main__':
 
-sep("*", 30)
 
-print(f'Your name is {name}.\nYour age is {age} years.')
-print(f'You live in {city}.')
+    name = input('What is your name?: ')
+    age = input('How old are you?: ')
+    city = input('Where do you live?: ')
+
+    sep("*", 30)
+
+    print(f'Your name is {name}.\nYour age is {age} years.')
+    print(f'You live in {city}.')
