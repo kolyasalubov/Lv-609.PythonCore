e58a1eb3a9b159076b1103803bc2107dd348e077
Volodymyr Cherney <volodymyrchyk@gmail.com>
2021-07-01 15:10:45 +0300

    HW1 ready

---
+++ HW1/Cherney/2.py
@@ -0,0 +1,8 @@
+a = int(input("Input variable a: "))
+b = int(input("Input variable b: "))
+print(f"a + b = {a+b}",
+      f"a - b = {a-b}",
+      f"a * b = {a*b}",
+      f"a / b = {a/b}",
+      f"a ** b = {a**b}",
+      sep = "\n")
