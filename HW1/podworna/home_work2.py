704ff344a0c7a2ee972775455b4cc1c97d1c1635
Podvorna Halyna <podworna1504@gmail.com>
2021-07-01 13:57:10 +0300

    added HW1

---
+++ HW1/podworna/home_work2.py
@@ -0,0 +1,8 @@
+a=int(input('Enter the value of a, a='))
+b=int(input('Enter the value of b, b='))
+
+print('a + b =',a+b)
+print('a - b =',a-b)
+print('a * b =',a*b)
+print('a / b =',a/b)
+print('a ** b =',a**b)
