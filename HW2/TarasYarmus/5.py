8971687be1d5583c9978c8aefe5313eabadc22d9
floyd <taras0@ukr.net>
2021-07-01 23:57:00 +0300

    added 5

--- HW2/TarasYarmus/5.py
+++ HW2/TarasYarmus/5.py
@@ -1,10 +1,18 @@
+#TASK ONE
 
-start = 0
-finish = 101
+number = input('Write four digits:')
+product = int(number[0]) * int(number[1]) * int(number[2]) * int(number[3])
 
+print ('Here result of product your four digits:', product)
 
-while start<finish:
-    print(start)
-    start+=1
-else:
-    print("Done!")
+#TASK TWO
+
+reverse = number[3] + number[2] + number[1] + number[0]
+
+print('Ok! Thats result of your reverse:', reverse)
+
+#TASK THREE
+
+last = sorted(number)
+
+print('Last one! Sorted your digits:', last)
