63af0a12e002caca1ebd17c40604b91eedb2d36f
Volodymyr Cherney <volodymyrchyk@gmail.com>
2021-07-01 17:42:48 +0300

    First assignment

---
+++ HW4/Cherney/1.py
@@ -0,0 +1,8 @@
+first_number = int(input("Enter first number "))
+second_number = int(input("Enter second number "))
+if first_number > second_number:
+    print("First number is bigger")
+elif first_number == second_number:
+    print("Numbers are equal")
+else:
+    print("Second number is bigger")
