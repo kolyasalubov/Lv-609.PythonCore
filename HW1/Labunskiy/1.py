7b4332a99ee48d2c1ae0fa2ca5ff9dad446e26c2
Roma Labunskiy <romalabunskiy13@gmail.com>
2021-07-02 16:51:32 +0300

    added first and second homework

--- HW1/Labunskiy/1.py
+++ HW1/Labunskiy/1.py
@@ -1,1 +1,16 @@
-a = 78
+
+name = input("What is your name? ")
+age = input("How old are you? ")
+city = input("Where do you live? ")
+
+print("Hello, %s!" % (name))
+print("Your age is % s!" % (age))
+print("You live in  %s!" % (city))
+
+print("Hello, {}!".format(name))
+print("Your age is {}!".format(age))
+print("You live in  {}!".format(city))
+
+print(f"Hello, {name}!")
+print(f"Your age is {age}!")
+print(f"You live in  {city}!")
---
+++ HW3/Labunskiy/1.py
@@ -0,0 +1,11 @@
+zen = import this
+count_better = zen.count("better")
+count_never = zen.count("never")
+counter_is = zen.count("is")
+zen_up = zen.upper()
+rep = zen.replace("i", "&")
+print(f"""Number of words better is {count_better},
+Number of words never is {count_never},
+Number of words is is {counter_is}""")
+print(zen_up)
+print(rep)
