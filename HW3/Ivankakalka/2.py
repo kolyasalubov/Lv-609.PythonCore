98a1c865c0adfb702fc0db34f187d7832b29933c
Ivankakalka <kivanka1551@gmail.com>
2021-07-03 23:24:13 +0300

    HW3/Ivankakalka/1.py

---
+++ HW3/Ivankakalka/2.py
@@ -0,0 +1,10 @@
+x = 6587
+c = map(int,str(x))
+y = list(c)
+multiply_y = y[0]*y[1]*y[2]*y[3]
+print (multiply_y)
+
+print (y[-1],y[-2],y[-3],y[-4])
+
+sorted_list = sorted (y)
+print (sorted_list)
---
+++ HW3/Ivankakalka/3.py
@@ -0,0 +1,4 @@
+a = 90
+b = 70
+a,b=b,a
+print (a,b)
