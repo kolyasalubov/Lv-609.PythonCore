6a1344eb3d47dcf2c40a99ac5be6b506ffea2cd6
bodacom <pischanuk.oik@gmail.com>
2021-07-03 23:06:33 +0300

    Headers changed to be able to distinguish between HW folders in VS code

--- HW1/bodacom/task2.py
+++ HW1/bodacom/task2.py
@@ -1,1 +1,1 @@
-#blank for the task 2 to add to the Git&GitHub repos
+#HW1 blank for the task 2 to add to the Git&GitHub repos
--- HW3/bodacom/task2.py
+++ HW3/bodacom/task2.py
@@ -1,1 +1,1 @@
-#blank for the task 1 to add to the Git&GitHub repos
+#HW3 blank for the task 1 to add to the Git&GitHub repos
