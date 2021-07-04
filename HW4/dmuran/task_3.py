7ec85b98b3ca3e51d60fce5748a887c5ef841f0c
Dima Muran <dismile444@gmail.com>
2021-07-04 22:46:04 +0300

    Corrected task_1 and task_2

--- HW4/dmuran/task_3.py
+++ HW4/dmuran/task_3.py
@@ -1,10 +1,11 @@
-fibonacci = 423
+fibonacci = 12
 result = 0
 first = 0
 second = 1
 print(first)
-while fibonacci >= result:
-    result = first + second
-    first = second
-    second = result
-    print(first)
+if fibonacci > 0:
+    while fibonacci >= result:
+        result = first + second
+        first = second
+        second = result
+        print(first)
