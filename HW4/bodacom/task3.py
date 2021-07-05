68451c2a7fed7855a72719d71c8196e10f8fed3d
bodacom <pischanuk.oik@gmail.com>
2021-07-05 23:52:02 +0300

    Some changes to HW4

--- HW4/bodacom/task3.py
+++ HW4/bodacom/task3.py
@@ -1,1 +1,9 @@
-#HW4 blank for the task 3 to add to the Git&GitHub repos
+# HW4 task 3
+
+int_number = int(input("Послідовність Фібоначі до якого елементу? "))
+print(int_number)
+
+fibonacci_element = None
+i = 0
+while i <= int_number:
+    fibonacci_element[i] = int(fibonacci_element[i-1]) + 
