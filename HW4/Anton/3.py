7dd11fae5119263cb598a8ecb05149719f2b0c39
last2nd <chtoxa@gmail.com>
2021-07-05 12:45:38 +0300

    added third task

---
+++ HW4/Anton/3.py
@@ -0,0 +1,13 @@
+# num = int(input('Enter number: '))
+num = 13
+first = 0
+second = 1
+result = 0
+num_list = [0,1]
+while result != num:
+    result = first + second
+    first = second
+    second = result
+    num_list.append(result)
+    
+print(num_list)
