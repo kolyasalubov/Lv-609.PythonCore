59ea55934129f6ec83a2ab4420ba2def95f1a156
Volodymyr Cherney <volodymyrchyk@gmail.com>
2021-07-06 01:34:49 +0300

    HW5 tasks from codewars

--- HW4/Cherney/task_1.py
+++
@@ -1,8 +0,0 @@
-final_number = int(input("Please input a number: "))
-i = 1 
-result = 1
-for i in range(1, final_number+1):
-    if i <= final_number:
-        result *= i
-        i += 1
-print(result)
---
+++ HW5/Cherney/banjo.py
@@ -0,0 +1,2 @@
+def are_you_playing_banjo(name):
+    return name + " plays banjo" if name[0] in ["R", "r"] else name + " does not play banjo" 
