63889629fa68b4cd982c60efceffee4acdb0e16d
Yura Voinov <yuravoinov@gmail.com>
2021-07-07 15:03:10 +0300

    Changed print in HW5(set)

--- HW5/yvoi/Set.py
+++ HW5/yvoi/Set.py
@@ -6,6 +6,4 @@ for x in range(1, 11):
     if x % 2 == 0: even.add(x)
     if x % 3 == 0: odd.add(x)
     if x % 2 != 0 and x % 3 != 0: no_divisible.add(x)
-print(even)
-print(odd)
-print(no_divisible)
+print(f"Even numbers: {even}\nOdd numbers: {odd}\nNot divisible on 2 and 3: {no_divisible}")
