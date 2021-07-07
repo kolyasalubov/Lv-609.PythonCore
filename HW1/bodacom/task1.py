954aefec2370e13ad1f5ba48309cc444abb1b1f4
bodacom <pischanuk.oik@gmail.com>
2021-07-07 23:27:01 +0300

    checked HW1, added some additional notifications

--- HW1/bodacom/task1.py
+++ HW1/bodacom/task1.py
@@ -1,11 +1,12 @@
-# HW1\bodacom\task1.py
+# HW1\bodacom\task1.py 
+# Done
 
 # Questionaire
 name = input('What is your name?: ')
 age = input('How old are you?: ')
 city = input('Where do you live?: ')
 
-# print infoa
+# Print info
 print("\nHello,", name)
 print("Your age is", age)
 print("You live in", city)
--- HW1/bodacom/task2.py
+++ HW1/bodacom/task2.py
@@ -1,12 +1,14 @@
-#HW1 blank for the task 2 to add to the Git&GitHub repos
+# HW1 task 2
+# Done
 
-# defining a and b as integers
+# Getting a and b as integers
 a = int(input('Define a: '))
 b = int(input('Define b: '))
 
-# printing results of different calculations
+# Printing results of different ariphmetic operations
 print('a + b = ', a + b)
 print('a - b = ', a - b)
 print('a * b = ', a * b)
 print('a / b = ', a / b)
 print('a ** b = ', a ** b)
+print()
