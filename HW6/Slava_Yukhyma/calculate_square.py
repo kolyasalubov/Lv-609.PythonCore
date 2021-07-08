36a2b9115f83674df1a280c5b7643a9a2b6484f1
Yaroslava Yukhyma <brak.fantaziyi@gmail.com>
2021-07-08 13:41:38 +0300

    added HW6

---
+++ HW6/Slava_Yukhyma/calculate_square.py
@@ -0,0 +1,25 @@
+
+
+def calculate_square():
+    figure = input('choose the figure: rectangle, triangle, circle: ')
+
+    if figure == 'rectangle':
+        h = int(input('Indicate height of the rectangle: '))
+        w = int(input('Indicate width of the rectangle: '))
+        print(f'Your rectangle is {w*h} cm2')
+        return w*h
+
+    if figure == 'triangle':
+        a = int(input('Write the base of your triangle: '))
+        h_triangle = int(input('Write the height of your triangle: '))
+        print(f'The square is {a*h_triangle*0.5}')
+        return a*h_triangle
+
+    if figure == 'circle':
+        r = int(input('Enter the radius of your circle'))
+        square_circle = 3, 14 * (r**2)
+        print(f'Your circle is {square_circle}sm2')
+        return square_circle
+
+
+calculate_square()
---
+++ HW6/Slava_Yukhyma/max_number.py
@@ -0,0 +1,5 @@
+def max_nubmer(*args):
+    return max(args)
+
+
+print(max_nubmer(1, 2, 3, 4, 5, 6, 7, 8))
