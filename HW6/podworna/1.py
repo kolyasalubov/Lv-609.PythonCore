42769a19b77254dba0be0659481406c90108c4a9
Podvorna Halyna <podworna1504@gmail.com>
2021-07-08 14:50:59 +0300

    added HW6

---
+++ HW6/podworna/1.py
@@ -0,0 +1,6 @@
+def returns_max_num(a,b):
+    """
+    function that returns the largest number of two numbers 
+    """
+    return max(a,b)
+#print(returns_max_num(4,7))
---
+++ HW6/podworna/3.py
@@ -0,0 +1,11 @@
+def calculates_number_char(string_input):
+   """
+   calculates the number of characters
+   """
+   set_string=set(string_input)
+   
+#    for i in len(string_input)
+st=input("string")
+print(set(st))
+e=enumerate(st)
+print(e)
