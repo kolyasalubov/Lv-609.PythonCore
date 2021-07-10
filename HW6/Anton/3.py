4fbc80f077a328cb1b9649dc9052852d99f5d20d
last2nd <chtoxa@gmail.com>
2021-07-10 14:48:08 +0300

    remade task

--- HW6/Anton/3.py
+++ HW6/Anton/3.py
@@ -1,14 +1,7 @@
-def count_of_characters(string):
-    return f'count of characters: {len(string)}' if type(string) == str else f'it "{string}" must be string'
+def counter(string):
+    """
+    Calculate count of elements in string.
 
-# SECOND VARIANT 
-
-# def count_of_characters(string):
-#     count = 0
-#     if type(string) == str:
-#         for i in string:
-#             count += 1
-#         return f'count of characters: {count}'
-#     else:
-#         return f'it "{string}" must be string'
-    
+    """
+    result = [f'Count of {elem} = {string.count(elem)}\n'for elem in set(string)] 
+    return "".join(result)
