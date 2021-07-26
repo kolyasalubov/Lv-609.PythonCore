836016d6829d4b48e5fc90ec5a8d6c0653108607
Tsutsuriak <tsutsurak@gmail.com>
2021-07-26 12:19:20 +0300

    Added HW

---
+++ HW7/Tsutsuriak/task1_modul1.py
@@ -0,0 +1,19 @@
+from math import pi
+
+def square_rectangle(length, width):
+    """
+    This function calculates the square of rectangle.
+    """
+    return(length * width)
+
+def square_triangle(height, base):
+    """
+    This function calculates the square of triangle.
+    """
+    return((height * base) / 2)
+
+def square_circle(radius):
+    """
+    This function calculates the square of circle.
+    """
+    return(pi * radius**2)
