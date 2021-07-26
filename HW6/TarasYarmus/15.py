f31b9ca3187d4b20457455716506dcb0cb0f1444
floyd <taras0@ukr.net>
2021-07-26 20:06:19 +0300

    remake HW

--- HW6/TarasYarmus/15.py
+++ HW6/TarasYarmus/15.py
@@ -5,19 +5,20 @@ password = input('Enter your password: ')
 
 while True:
     if not re.search('[a-z]', password):
-        print('Valid password!')
+        print('Not valid password! Try again!')
         break
     elif not re.search('[A-Z]', password):
-        print('Valid password!')
+        print('Not valid password! Try again!')
         break
     elif not re.search('[0-9]', password):
-        print('Valid password!')
+        print('Not valid password! Try again!')
         break
     elif not re.search('[$#@]', password):
-        print('Valid password!')
+        print('Not valid password! Try again!')
         break
     elif (len(password)<6 or len(password)>16):
-        print('Valid password!')
+        print('Not valid password! Try again!')
         break
     else:
-        print('Not valid password! Try again!')
+        print('Valid password!')
+        break
