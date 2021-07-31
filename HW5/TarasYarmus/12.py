5b63a933f21af7e545f8cb2050c991a531ba3b32
floyd <taras0@ukr.net>
2021-07-31 12:13:24 +0300

    remake 12

--- HW5/TarasYarmus/12.py
+++ HW5/TarasYarmus/12.py
@@ -1,27 +1,3 @@
-def rectangle(arg1, arg2):
-    '''
-    This function calculate square of the rectangle
-    Type: float
-    Input: your arguments
-    '''
-    return float(arg1*arg2)
-
-
-def triangle(height, basis):
-    '''
-    This function calculate square of the triangle
-    Type: float
-    Input: your arguments'''
-    return float(height/2)*basis
-
-
-def circle(r):
-    '''
-    This function calculate square of the circle
-    Type: float
-    Input: enter number for first argument "r"'''
-    return float(3.14*r)**2
-
 question = input(
     '''
     Hello! I will calculate square by your arguments! 
@@ -30,19 +6,44 @@ question = input(
     Calculate square for circle: press 3.
     Make your choice: ''')
 
+if question != '1' and question != '2' and question != '3':
+    print('Wrong option! Try again!')
+
+def rectangle(arg1, arg2):
+    '''
+    This function calculate square of the rectangle
+    Type: float
+    Input: your arguments
+    '''
+    return float(arg1*arg2)
+
 if question == '1':
     rectangle1 = float(input('Enter first side length: '))
     rectangle2 = float(input('Enter second side length: '))
     result_rectangle = rectangle(rectangle1, rectangle2)
     print(f'Square of your rectangle is: {result_rectangle}')
-elif question == '2':
+
+def triangle(height, basis):
+    '''
+    This function calculate square of the triangle
+    Type: float
+    Input: your arguments'''
+    return float(height/2)*basis
+
+if question == '2':
     triangle1 = float(input('Enter your height: '))
     triangle2 = float(input('Enter your basis: '))
     result_triangle = triangle(triangle1, triangle2)
     print(f'Square of your triangle is: {result_triangle}')
-elif question == '3':
+
+def circle(r):
+    '''
+    This function calculate square of the circle
+    Type: float
+    Input: enter number for first argument "r"'''
+    return float(3.14*r)**2
+
+if question == '3':
     circle1 = float(input('Enter numeric for "r": '))
     result_circle = circle(circle1)
     print(f'Square of your circle is: {result_circle}')
-else:
-    print('Wrong option! Try again!')
