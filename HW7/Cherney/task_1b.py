49b217f19f36edf54fe85c956becd29d841849b4
Volodymyr Cherney <volodymyrchyk@gmail.com>
2021-08-02 15:22:41 +0300

    HW9 and HW10

--- HW7/Cherney/task_1b.py
+++ HW7/Cherney/task_1b.py
@@ -1,11 +1,1 @@
-import math
-
-def calc_rectan_area(a,b):
-    return a*b
-
-def calc_triangle_area(a,b):
-    return a*b*0.5
-
-def calc_circle_area(r):
-    return math.pi*pow(r,2)
-
+import this
---
+++ HW9/double_char.py
@@ -0,0 +1,5 @@
+def doub(s):
+    lis = []
+    for i in s:
+        lis.append(i*2)
+    return "".join(lis)
