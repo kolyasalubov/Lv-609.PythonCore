9b0f04873054e6a9caa0e522310c02c2b1336016
Larysa Lomyha <lomygal@gmail.com>
2021-08-11 17:54:40 +0300

    added homework_3

---
+++ HW3/LarysaLomyha/2.py
@@ -0,0 +1,6 @@
+number_1 = input(str())
+number_2 = number_1[::-1]
+print(number_2)
+print(int(number_1[0])*int(number_1[1])*int(number_1[2])*int(number_1[3]))
+number_sort = "".join(sorted(number_1))
+print(number_sort)
---
+++ HW3/LarysaLomyha/3.py
@@ -0,0 +1,5 @@
+variable_1 = int(12)
+variable_2 = 'old'
+print(variable_1, variable_2)
+variable_1, variable_2 = variable_2, variable_1
+print(variable_1, variable_2)
